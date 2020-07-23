b365762911d021511f4b91bb46ad7c083e212d47
chuck catron <charles.catron@gmail.com>
2020-07-23 07:21:26 -0400

    fix auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {AmplifyAngularModule, AmplifyService } from 'aws-amplify-angular';
+import Amplify from 'aws-amplify';
+import awsconfig from '../aws-exports';
+
+/* Configure Amplify resources */
+Amplify.configure(awsconfig);
 
 @NgModule({
   declarations: [
